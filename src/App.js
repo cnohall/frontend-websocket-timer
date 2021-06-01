914ed146ceb5188fefa2c5f0db83807c2c7fb07b
cnohall <christopher.nohall@gmail.com>
2021-06-01 10:24:41 +0900

    add meeting ending time

--- src/App.js
+++ src/App.js
@@ -8,30 +8,47 @@ const ENDPOINT = "https://backend-websocket-timer.herokuapp.com/";
 // const ENDPOINT = "http://localhost:4001";
 const socket = socketIOClient(ENDPOINT, {transports: ['websocket']});
 
+//Time variables
+const second = 1000; //in milliseconds
+const min = 60 * second; 
+const hour = 60 * min;
+
 function App() {
   const [response, setResponse] = useState("");
   const [startTime, setStartTime] = useState("Not started yet");
+  const [shouldEnd, setShouldEnd] = useState("");
 
   useEffect(() => {
     socket.on("FromAPI", data => {
-      setResponse(data);
+      const serverTime = new Date(data);
+      const currentTime = new Date(serverTime/1000 * 1000).toISOString().substr(11, 8)
+      setResponse(currentTime);
     });
     socket.on("start", data => {
-      setStartTime("The meeting started " + data);
+      const currentTime = new Date(data);
+      const meetingLength = hour + (45 * min); // 105 minutes or 1 hour and 45 minutes
+      const start = new Date(currentTime/1000 * 1000).toISOString().substr(11, 8)
+      const end = new Date((currentTime/1000 * 1000) + meetingLength).toISOString().substr(11, 8)
+      setStartTime("The meeting started at " + start);
+      setShouldEnd("The meeting should end at " + end);
     });
     socket.on("stop", data => {
-      setStartTime("The meeting stopped");
+      setStartTime("The meeting ended");
+      setShouldEnd("");
     });
   }, []);
 
   return (
     <Container>
-      <h3 className="m-5 text-center">
+      <h4 className="m-5 text-center">
         <time dateTime={response}>Current time UTC: {response}</time>
-      </h3>
-      <h3 className="m-5 text-center">
+      </h4>
+      <h4 className="mx-5 text-center">
         <time dateTime={startTime}>{startTime}</time>
-      </h3>
+      </h4>
+      <h4 className="mb-5 text-center">
+        <time dateTime={shouldEnd}>{shouldEnd}</time>
+      </h4>
       <Row className="justify-content-center">
         <Button onClick={ () => socket.emit("start")} className='mx-3 btn-lg'>Start</Button>
         <Button onClick={ () => socket.emit("stop")} className='mx-3 btn-lg'>Stop</Button>
